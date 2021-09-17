1eb68b8b90f193ee2ac208b8893b6030f5524990
Quinstein <quinstein900@gmail.com>
2021-09-17 22:45:17 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const mongoose = require('mongoose');
+const bodyparser = require("body-parser");
+mongoose.connect('mongodb://localhost:27017/contactDance', {useNewUrlParser: true, useUnifiedTopology: true});
+const port = 8000;
+
+
+// Define Mongoose Schema
+const contactSchema = new mongoose.Schema({
+  name: String,
+  phone: String,
+  email: String,
+  address: String,
+  desc: String
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+
+// app.use(express.static('static', options))
+
+//EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
+app.use(express.urlencoded({ extended: true }))
+
+//PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+
+//ENDPOINTS
+app.get('/', (req, res)=>{
+    const params = { }
+    res.status(200).render('home.pug', params);
+})
+app.get('/contact', (req, res)=>{
+    const params = { }
+    res.status(200).render('contact.pug', params);
+})
+
+app.post('/contact', (req, res)=>{
+    var myData = new Contact(req.body);
+    myData.save().then(()=>{
+        res.send("This item has been saved to the database")
+    }).catch(()=>{
+        res.send(200).render('contact.pug', params);
+    })
+    
+})
+
+//START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
